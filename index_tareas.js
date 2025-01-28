4581fd26c69a9f4d9997a71cf5ec1048c1d4ee15
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-01-28 20:39:37 -0300

    get y post con prisma

--- index_tareas.js
+++ index_tareas.js
@@ -1,7 +1,10 @@
+const { PrismaClient } = require('@prisma/client')
 const express = require("express")
 const cors = require('cors')
 const app = express()
 const port = 3000
+const prisma = new PrismaClient()
+
 app.use(cors())
 app.use(express.json())
 
@@ -11,31 +14,37 @@ app.get('/', (req, res) => {
     res.send('Mis tareas')
 })
 
-app.get('/api/v1/tareas', (req, res) => {
+app.get('/api/v1/tareas', async (req, res) => {
+    const tareas = await prisma.tarea.findMany()
     res.json(tareas)
 })
 
-app.get('/api/v1/tareas/:id', (req, res) => {
-    const tarea = tareas.find((element) => element.id == req.params.id)
+app.get('/api/v1/tareas/:id', async (req, res) => {
+    const tarea = await prisma.tarea.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
 
-    if (tarea === undefined) {
+    if (tarea === null) {
         res.sendStatus(404)
         return
     }
+
     res.json(tarea)
 })
 
-app.post('/api/v1/tareas', (req, res) => {
-    const tarea = {
-        id: tareas.length + 1,
-        nombre: req.body.nombre,
-        descripcion: req.body.descripcion,
-        fecha_inicio: req.body.inicio_tarea,
-        fecha_fin: req.body.fin_tarea
-        // objetivos: req.body.objetivos ?? '',
-        // progreso: req.body.progreso ?? '0%'
-    }
-    tareas.push(tarea)
+app.post('/api/v1/tareas', async (req, res) => {
+    const tarea = await prisma.tarea.create({
+        data: {
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            inicio: req.body.inicio_tarea,
+            fin: req.body.fin_tarea,
+            id_objetivo: req.body.id_objetivo,
+            estado: req.body.estado
+        }
+    })
     res.status(201).send(tarea)
 })
 
