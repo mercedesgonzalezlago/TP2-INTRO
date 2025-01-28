2d85156b36bfd55c3fc65b50cac126a9408328f6
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-01-28 20:46:58 -0300

    tabla de tareas

---
+++ prisma/migrations/20250128234535_change/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `estado` on the `Tarea` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Tarea" DROP COLUMN "estado";
