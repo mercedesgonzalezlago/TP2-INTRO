5d13e979e5c11de255db4af14e982044347d5b02
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-01-28 20:46:36 -0300

    delete y put con prisma

--- index_tareas.js
+++ index_tareas.js
@@ -41,39 +41,58 @@ app.post('/api/v1/tareas', async (req, res) => {
             descripcion: req.body.descripcion,
             inicio: req.body.inicio_tarea,
             fin: req.body.fin_tarea,
-            id_objetivo: req.body.id_objetivo,
-            estado: req.body.estado
+            id_objetivo: req.body.id_objetivo
         }
     })
     res.status(201).send(tarea)
 })
 
-app.delete('/api/v1/tareas/:id', (req, res) => {
-    const tarea = tareas.find((element) => element.id == req.params.id)
+app.delete('/api/v1/tareas/:id', async (req, res) => {
+    const tarea = await prisma.tarea.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
 
-    if (tarea === undefined) {
-        res.sendStatus(404)
+    if (tarea === null) {
+        res,sendStatus(404)
         return
     }
 
-    tareas = tareas.filter((element) => element.id !== req.params.id)
+    await prisma.tarea.delete({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
+
     res.send(tarea)
 })
 
-app.put('/api/v1/tareas/:id', (req, res) => {
-    let tarea_id = tareas.findIndex((element) => element.id == req.params.id)
+app.put('/api/v1/tareas/:id', async (req, res) => {
+    const tarea = await prisma.tarea.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
 
-    if (tarea_id === -1) {
-        res.sendStatus(404)
+    if (tarea === null) {
+        res,sendStatus(404)
         return
     }
 
-    tareas[tarea_id].nombre = req.body.nombre ?? tareas[tarea_id].nombre
-    tareas[tarea_id].descripcion = req.body.descripcion ?? tareas[tarea_id].descripcion
-    tareas[tarea_id].fecha_inicio = req.body.inicio_tarea ?? tareas[tarea_id].fecha_inicio
-    tareas[tarea_id].fecha_fin = req.body.fin_tarea ?? tareas[tarea_id].fecha_fin
+    await prisma.tarea.update({
+        where: {
+            id: tarea.id
+        },
+        data : {
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            inicio: req.body.inicio_tarea,
+            fin: req.body.fin_tarea
+        }
+    })
 
-    res.send(tareas[tarea_id])
+    res.send(tarea)
 })
 
 app.listen(port, () => {
