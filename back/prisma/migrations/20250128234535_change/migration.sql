5b380969f2e199473c1780b1b9a690adcc256dfe
Mercedes González Lago <mmgonzalez@fi.uba.ar>
2025-02-03 20:53:35 -0300

    barra de navegacion

--- back/prisma/migrations/20250128234535_change/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `estado` on the `Tarea` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Tarea" DROP COLUMN "estado";
