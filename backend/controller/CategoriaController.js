eb7eac30e7edd22a7596104d83ebf03ac8652d46
Carlos <carlos.itbone@gmail.com>
2025-02-03 23:08:23 -0300

    actualizacion de controller con la info correspondiente a cada entidad

--- backend/controller/CategoriaController.js
+++ backend/controller/CategoriaController.js
@@ -1,12 +1,12 @@
 import {Categoria} from "../model/CategoriaModel.js";
 
 export const crearCategoria = async (req, res) => {
-    const { titulo, descripcion, customer_id, categoria_id } = req.body;
+    const { nombre, descripcion } = req.body;
     try {
-        const result = await Categoria.crearCategoria(titulo, descripcion, customer_id, categoria_id);
-        res.status(201).json({ id: result.insertId, nombre, email });
+        const result = await Categoria.crearCategoria(nombre, descripcion);
+        res.status(201).json({ id: result.insertId, nombre, descripcion });
     } catch (err) {
-        res.status(500).json({ error: 'Error al crear el cliente' });
+        res.status(500).json({ error: 'Error al crear la Categoria' });
     }
 };
 
@@ -15,7 +15,7 @@ export const obtenerCategoria = async (req, res) => {
         const results = await Categoria.obtenerCategorias();
         res.status(200).json(results);
     } catch (err) {
-        res.status(500).json({ error: 'Error al obtener los clientes' });
+        res.status(500).json({ error: 'Error al obtener las Categoria' });
     }
 };
 
@@ -24,25 +24,25 @@ export const obtenerCategoriaPorId = async (req, res) => {
     try {
         const result = await Categoria.obtenerCategoriaPorId(id);
         if (result.length === 0) {
-            return res.status(404).json({ error: 'Cliente no encontrado' });
+            return res.status(404).json({ error: 'Categoria no encontrada' });
         }
         res.status(200).json(result[0]);
     } catch (err) {
-        res.status(500).json({ error: 'Error al obtener el cliente' });
+        res.status(500).json({ error: 'Error al obtener la Categoria' });
     }
 };
 
 export const actualizarCategoria = async (req, res) => {
     const { id } = req.params;
-    const { name, email } = req.body;
+    const { name, descripcion } = req.body;
     try {
-        const result = await Categoria.actualizarCategoria(id, name, email);
+        const result = await Categoria.actualizarCategoria(id, name, descripcion);
         if (result.affectedRows === 0) {
-            return res.status(404).json({ error: 'Cliente no encontrado' });
+            return res.status(404).json({ error: 'Categoria no encontrada' });
         }
-        res.status(200).json({ message: 'Cliente actualizado' });
+        res.status(200).json({ message: 'Categoria actualizada' });
     } catch (err) {
-        res.status(500).json({ error: 'Error al actualizar el cliente' });
+        res.status(500).json({ error: 'Error al actualizar la categoria' });
     }
 };
 
@@ -51,11 +51,11 @@ export const eliminarCategoria = async (req, res) => {
     try {
         const result = await Categoria.eliminarCategoria(id);
         if (result.affectedRows === 0) {
-            return res.status(404).json({ error: 'Cliente no encontrado' });
+            return res.status(404).json({ error: 'Categoria no encontrada' });
         }
-        res.status(200).json({ message: 'Cliente eliminado' });
+        res.status(200).json({ message: 'Categoria eliminada' });
     } catch (err) {
-        res.status(500).json({ error: 'Error al eliminar el cliente' });
+        res.status(500).json({ error: 'Error al eliminar la Categoria' });
     }
 };
 
